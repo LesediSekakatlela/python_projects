0cbee165f6748eadfd5914fa46e1a7bcedbce6ca
Lesedi <leseditumelo32@gmail.com>
2021-06-17 16:52:00 +0200

    multilineplot

---
+++ Matplotlib intro/Intro/multilineplot.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+
+
+x1 = [1,2,3,4,5]
+y1 = [1,2,4,8,16]
+
+x2 = [1,2,3,4,5]
+y2 = [1,3,9,13,16]
+
+x3 = [1,2,3,4,5]
+y3 = [2,4,6,8,10]
+
+
+
+plt.plot(x1, y1, 'g^-', label = 'students')
+plt.plot(x2, y2, 'bo--', label = 'grades')
+plt.plot(x3, y3, 'r-', label = 'visitors')
+plt.legend(loc='best')
+
+
+plt.title('Your title')
+plt.xlabel('Horizontal title')
+plt.ylabel('Vertical title')
+plt.show()
