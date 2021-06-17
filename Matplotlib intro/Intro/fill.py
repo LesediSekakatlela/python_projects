8860657e3266a168d4ce765edae322ef5daea7df
Lesedi <leseditumelo32@gmail.com>
2021-06-17 16:57:25 +0200

    fillplot

--- Matplotlib intro/Intro/fill.py
+++ Matplotlib intro/Intro/fill.py
@@ -3,9 +3,11 @@ import numpy as np
 
 X = np.arange(0,10)
 Y1 = 0.05*X*X
+Y2 = 0.03*X*X
 
 
 plt.ylim(0, 5)
-plt.plot(X, Y1, color='blue')
-plt.fill_between(X, Y1, color='red', alpha=0.5)
+plt.plot(X, Y1, color='green')
+plt.plot(X, Y2, color='red')
+plt.fill_between(X, Y1, Y2, color='blue', alpha=0.5)
 plt.show()
